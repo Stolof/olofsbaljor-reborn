ac83db84f3646ac3ad168289e8b01f1b1dbe9804
Stolof <olof.marklund@valtech.com>
2020-10-10 10:30:10 +0200

    Added unit to nutrients

--- components/NutrientTableEntry.js
+++ components/NutrientTableEntry.js
@@ -4,7 +4,11 @@ import nutrientStyle from '../styles/Nutrients.module.css'
      return (
           <div className={nutrientStyle.tableRow}>
              <div>{props.name}</div>
-             <div className={nutrientStyle.tableRowAmount}>{Math.round((props.value + Number.EPSILON) * 100) / 100 }</div>
+             
+             <div className={nutrientStyle.tableRowAmount}>
+             <p> {Math.round((props.value + Number.EPSILON) * 100) / 100 } {props.name === "Calories" ? "kcal" :  "g" }
+              </p>
+             </div>
          </div>
      )
    }
